92ab30f25a41cebc347344aec939b971d8cf6dd9
Ksenia Agalakova <agalaksu111@gmail.com>
2025-03-19 23:02:31 +0300

    Removed menu button on large screens

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,24 +4,39 @@ import { AppBar, Toolbar, IconButton, Typography, Badge } from "@mui/material";
 import MenuIcon from "@mui/icons-material/Menu";
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 import { CartContext } from "../contexts/CartContext";
+import { useRouter } from "next/navigation";
+import { SidebarContext } from "@/contexts/SidebarContext";
 
 const Header = () => {
   const { totalPrice } = useContext(CartContext);
+  const { handleDrawerToggle } = useContext(SidebarContext);
+  const router = useRouter();
+
+  const handleNavigate = () => {
+    router.push("/");
+  };
 
   return (
     <header>
       <AppBar position="static" sx={{ backgroundColor: "#232f3e" }}>
         <Toolbar>
-          <IconButton size="large" edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2, display: { xs: "block", sm: "block", md: "block", lg: "none" } }}
+            onClick={handleDrawerToggle}
+          >
             <MenuIcon />
           </IconButton>
 
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1, cursor: "pointer" }} onClick={handleNavigate}>
             My Store
           </Typography>
 
           <IconButton size="large" color="inherit" aria-label="cart">
-            <Badge badgeContent={totalPrice > 0 ? totalPrice.toFixed(2) + "$" : 0 + "$"} color="secondary">
+            <Badge badgeContent={totalPrice > 0 ? totalPrice.toFixed(2) + "$" : 0 + "$"} color="error">
               <ShoppingCartIcon />
             </Badge>
           </IconButton>
