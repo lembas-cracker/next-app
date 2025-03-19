c4d87761575a0bf1a53c773a14470356760f9990
Ksenia Agalakova <agalaksu111@gmail.com>
2025-03-19 23:05:05 +0300

    Added SingleProduct component

---
+++ src/components/SingleProduct.tsx
@@ -0,0 +1,57 @@
+import React from "react";
+import { Product } from "./ProductCard";
+import { Box, Divider, Typography } from "@mui/material";
+import Image from "next/image";
+import styles from "../styles/SingleProduct.module.scss";
+
+type ProductDetailsProps = {
+  product: Product;
+};
+
+const SingleProduct: React.FC<ProductDetailsProps> = ({ product }) => {
+  return (
+    <>
+      <Box
+        sx={{
+          maxWidth: "fitContent",
+          p: 3,
+          display: "flex",
+          justifyContent: "start",
+          gap: 5,
+          m: 3,
+        }}
+        className={styles.singleProduct}
+      >
+        <Box sx={{ width: "350px", height: "auto" }} className={styles.imageContainer}>
+          <Image src={product.image} alt={product.title} layout="responsive" width={350} height={350} />
+        </Box>
+        <Box sx={{ px: 3, display: "flex", flexDirection: "column" }} className={styles.detailsContainer}>
+          <Typography variant="h4" component="h1" gutterBottom>
+            {product.title}
+          </Typography>
+
+          <Typography sx={{}} variant="h6" component="p" gutterBottom>
+            ${product.price.toFixed(2)}
+          </Typography>
+
+          <Divider sx={{ my: 3 }}></Divider>
+
+          <Typography variant="h6" component="h3" gutterBottom>
+            Описание
+          </Typography>
+          <Typography variant="body1" component="p" gutterBottom>
+            {product.description}
+          </Typography>
+
+          <Divider sx={{ my: 3 }}></Divider>
+
+          <Typography variant="h6" component="h3" gutterBottom>
+            0 Отзывов
+          </Typography>
+        </Box>
+      </Box>
+    </>
+  );
+};
+
+export default SingleProduct;
